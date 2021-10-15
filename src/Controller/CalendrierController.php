24b494ca71dbf42dabde9c538e961813a4fd3b8c
marwen <mansourmarwen@outlook.fr>
2021-10-15 15:26:31 +0200

    formulaire_ajout

--- src/Controller/CalendrierController.php
+++ src/Controller/CalendrierController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\FruitsLegumes;
+use App\Form\FruitsLegumesType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CalendrierController extends AbstractController
 {
     #[Route('/calendrier', name: 'calendrier')]
-    public function index(): Response
+    public function affiche(): Response
     {
         return $this->render('calendrier/calendrier.html.twig', [
             'controller_name' => 'CalendrierController',
         ]);
+    }
+    #[Route('/calendrier/create', name: 'create')]
+    public function creer(Request $request,EntityManagerInterface $entityManager ): Response
+    {
+        $produit = new FruitsLegumes();
+
+        $form = $this->createForm(FruitsLegumesType::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('calendrier', []);
+        }
+        // else
+        // {
+        //     return $this->redirectToRoute('create', []);
+        // }
+        return $this->render('calendrier/add.html.twig', [
+            'formulaire' => $form->createView()
+            ]);
     }
 }
--- src/Entity/FruitsLegumes.php
+++ src/Entity/FruitsLegumes.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use App\Repository\FruitsLegumesRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=FruitsLegumesRepository::class)
+ * @Vich\Uploadable
  */
 class FruitsLegumes
 {
@@ -41,6 +44,19 @@ class FruitsLegumes
      * @ORM\Column(type="string", length=255)
      */
     private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+    
 
     public function getId(): ?int
     {
@@ -105,5 +121,33 @@ class FruitsLegumes
         $this->type = $type;
 
         return $this;
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
     }
 }
