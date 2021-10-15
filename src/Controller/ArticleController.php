945fd01904f5094082aeb222e536f3361c3d3dac
marwen <mansourmarwen@outlook.fr>
2021-10-15 10:31:53 +0200

    articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,9 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/les_bonnes_pratiques', name: 'pratiques')]
-    public function index(): Response
+    public function bonnes_pratiques(): Response
     {
         return $this->render('article/bonnes_pratiques.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    #[Route('/la_diversification_alimentaire', name: 'diversification')]
+    public function diversification_alimentaire(): Response
+    {
+        return $this->render('article/diversification_alimentaire.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
