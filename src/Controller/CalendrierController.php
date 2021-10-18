57d496c4a92e264abd87305e12153d5fff5e9313
marwen <mansourmarwen@outlook.fr>
2021-10-18 20:16:54 +0200

    insertion des fruits et légumes modif

--- src/Controller/CalendrierController.php
+++ src/Controller/CalendrierController.php
@@ -16,19 +16,57 @@ class CalendrierController extends AbstractController
     #[Route('/calendrier', name: 'calendrier')]
     public function affiche(FruitsLegumesRepository $repository): Response
     {
-        $legumes = $repository->findBy(
-                array('type' =>'légumes'),
+        $legumes_hiv = $repository->findBy(
+                array('type' =>'légumes','saison' =>'hiver')
 
-            );;
-        $fruits = $repository->findBy(
-                array('type' =>'fruits'),
+            );
+        $fruits_hiv = $repository->findBy(
+                array('type' =>'fruits','saison' =>'hiver')
 
-            );;
+            );
+
+        $legumes_prin = $repository->findBy(
+                array('type' =>'légumes','saison' =>'printemps')
+
+            );
+        $fruits_prin = $repository->findBy(
+                array('type' =>'fruits','saison' =>'printemps')
+
+            );
+
+        $legumes_ete = $repository->findBy(
+                array('type' =>'légumes','saison' =>'ete')
+
+            );
+        $fruits_ete = $repository->findBy(
+                array('type' =>'fruits','saison' =>'ete')
+
+            );
+
+        $legumes_aut = $repository->findBy(
+                array('type' =>'légumes','saison' =>'automne')
+
+            );
+        $fruits_aut = $repository->findBy(
+                array('type' =>'fruits','saison' =>'automne')
+
+            );
         
         return $this->render('calendrier/calendrier.html.twig', [
             'controller_name' => 'CalendrierController',
-            'fruits' =>$fruits,
-            'legumes'=>$legumes
+            'fruits_hiv' =>$fruits_hiv,
+            'legumes_hiv'=>$legumes_hiv,
+
+            'fruits_prin' =>$fruits_prin,
+            'legumes_prin'=>$legumes_prin,
+
+            'fruits_ete' =>$fruits_ete,
+            'legumes_ete'=>$legumes_ete,
+
+            'fruits_aut' =>$fruits_aut,
+            'legumes_aut'=>$legumes_aut
+
+            
         ]);
 
         
